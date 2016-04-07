9ebcc524a8723b13c80d920352829b0657f0442b
boss <fabki00@gmail.com>
2016-04-07 00:41:24 -0400

    images

--- app/src/main/java/com/android/msf/kingsofpoland/MainActivity.java
+++ app/src/main/java/com/android/msf/kingsofpoland/MainActivity.java
@@ -2,6 +2,9 @@ package com.android.msf.kingsofpoland;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void goHome(View view) {
+        ImageView imageView = (ImageView) findViewById(R.id.main_img);
+        imageView.setImageResource(R.drawable.kingsofpoland);
+
+        TextView textView = (TextView) findViewById(R.id.king_name);
+        textView.setText("");
+    }
+
+    public void showPiast(View view) {
+
+    }
+
 }
