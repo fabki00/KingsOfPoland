49cb21cc525dabc494774eccc69e8ffb6db31573
boss <fabki00@gmail.com>
2016-04-08 19:08:02 -0400

    Added second activity for Piast Dynasty

--- app/src/main/java/com/android/msf/kingsofpoland/MainActivity.java
+++ app/src/main/java/com/android/msf/kingsofpoland/MainActivity.java
@@ -1,8 +1,10 @@
 package com.android.msf.kingsofpoland;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,14 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void goHome(View view) {
-        ImageView imageView = (ImageView) findViewById(R.id.main_img);
-        imageView.setImageResource(R.drawable.kingsofpoland);
 
-        TextView textView = (TextView) findViewById(R.id.king_name);
-        textView.setText("");
+        Button buttonPiast = (Button) findViewById(R.id.bPiast);
+        buttonPiast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // code to start new activity
+                Intent intent = new Intent(MainActivity.this, Piast.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+//    public void goHome(View view) {
+//        ImageView imageView = (ImageView) findViewById(R.id.main_img);
+//        imageView.setImageResource(R.drawable.kingsofpoland);
+//
+//        TextView textView = (TextView) findViewById(R.id.king_name);
+//        textView.setText("");
+//    }
 
     public void showPiast(View view) {
 
